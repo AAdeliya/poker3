1d1af2c0676bcd1ef4a3e9eb126a7935d6b91240
firstPush <adel.sdet2@gmail.com>
2024-12-14 23:51:48 -0600

    poker game class for 36/52 cards

--- src/main/java/org/game/model/PokerGame.java
+++ src/main/java/org/game/model/PokerGame.java
@@ -1,15 +1,44 @@
 package org.game.model;
 
+
 import java.util.Comparator;
 import java.util.List;
 
+
 public class PokerGame {
     public String determineWinner(List<Card> player1Hand, List<Card> player2Hand) {
+        PokerHandEvaluator evaluator = new PokerHandEvaluator();
 
+
+        HandRank player1Rank = evaluator.evaluateHand(player1Hand);
+        HandRank player2Rank = evaluator.evaluateHand(player2Hand);
+
+
+        if (player1Rank.ordinal() < player2Rank.ordinal()) {
+            return "Player 1 wins!";
+        } else if (player1Rank.ordinal() > player2Rank.ordinal()) {
+            return "Player 2 wins!";
+        } else {
+            // Tie-breaking logic
+            return compareHighCards(player1Hand, player2Hand);
         }
     }
 
-    private String compareHighCards(List<Card> player1Hand, List<Card> player2Hand) {
 
+    private String compareHighCards(List<Card> player1Hand, List<Card> player2Hand) {
+        // Sort hands by rank in descending order
+        player1Hand.sort(Comparator.comparing(Card::getRank).reversed());
+        player2Hand.sort(Comparator.comparing(Card::getRank).reversed());
+
+
+        for (int i = 0; i < Math.min(player1Hand.size(), player2Hand.size()); i++) {
+            if (player1Hand.get(i).getRank().ordinal() > player2Hand.get(i).getRank().ordinal()) {
+                return "Player 1 wins!";
+            } else if (player1Hand.get(i).getRank().ordinal() < player2Hand.get(i).getRank().ordinal()) {
+                return "Player 2 wins!";
+            }
+        }
+        return "It's a tie!";
     }
 }
+
--- src/main/java/poker52upTo10players/model/PokerGame.java
+++ src/main/java/poker52upTo10players/model/PokerGame.java
@@ -1,9 +1,39 @@
+
 package poker52upTo10players.model;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PokerGame {
-        public Player determineWinner(List<Player> players, List<Card> communityCards) {
 
+public class PokerGame {
+    public Player determineWinner(List<Player> players, List<Card> communityCards) {
+        Player winner = null;
+        HandRank bestRank = null;
+
+
+        PokerHandEvaluator evaluator = new PokerHandEvaluator();
+
+
+        for (Player player : players) {
+            List<Card> combinedHand = combinePlayerAndCommunityCards(player.getHand(), communityCards);
+            HandRank rank = evaluator.evaluateHand(combinedHand);
+
+
+            if (bestRank == null || rank.ordinal() < bestRank.ordinal()) {
+                bestRank = rank;
+                winner = player;
+            }
         }
+
+
+        return winner;
     }
+
+
+    private List<Card> combinePlayerAndCommunityCards(List<Card> playerHand, List<Card> communityCards) {
+        List<Card> combined = new ArrayList<>(playerHand);
+        combined.addAll(communityCards);
+        return combined;
+    }
+}
+
+
